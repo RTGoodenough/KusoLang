b2fed73e95aff45a93d3dcb0fdaeaf06c2f9aedd
Rolland Goodenough <goodenoughr@gmail.com>
2023-12-19 19:39:14 -0600

    adding license comments

--- include/generator/context.hpp
+++ include/generator/context.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file context.hpp
+ * @author Rolland Goodenough (goodenoughr@gmail.com)
+ * @date 2023-12-19
+ *
+ * @copyright Copyright 2023 Rolland Goodenough
+ *
+ * This file is part of kuso which is released under the MIT License
+ * See file LICENSE for the full License
+ */
+
 #pragma once
 
 #include <map>
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,4 +1,13 @@
-
+/**
+ * @file parser.cpp
+ * @author Rolland Goodenough (goodenoughr@gmail.com)
+ * @date 2023-12-19
+ *
+ * @copyright Copyright 2023 Rolland Goodenough
+ *
+ * This file is part of kuso which is released under the MIT License
+ * See file LICENSE for the full License
+ */
 
 #include <algorithm>
 #include <initializer_list>
